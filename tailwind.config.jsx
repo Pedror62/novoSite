0643f9630101d7dd77de064e65b94ac1d4eeed6e
Pedro Henrique <102195011+Pedror62@users.noreply.github.com>
2025-12-03 17:20:35 -0300

    Add files via upload

---
+++ tailwind.config.jsx
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    // Garanta que esta linha inclua seus arquivos .jsx
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
